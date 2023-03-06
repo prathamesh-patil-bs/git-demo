626b42cc226b08443c89c48f568ea388d68d14f3
Prathamesh Patil <prathmesh@bigscal.com>
2023-03-06 15:57:37 +0530

    added feature 3

--- src/app.js
+++ src/app.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => {
 
 app.post('/feature1', (req, res) => {
   res.send('FEATURE 1 COMPLETED');
+});
+
+app.put('/', (req, res) => {
+  res.send('Feature 3');
 })
 
 app.listen(4000, () => { console.log('Server is up and running at 4000') });
